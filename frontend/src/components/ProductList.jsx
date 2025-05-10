ebbdd2c5936920c93809e1cae6fb7194584517ab
Growza3 <arpitap9005@gmail.com>
2025-05-11 00:27:20 +0530

    Necessary Updates Done

--- frontend/src/components/ProductList.jsx
+++ frontend/src/components/ProductList.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+
 const ProductList = ({ searchTerm }) => {
   const [products, setProducts] = useState([]);
+  
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/api/products');
+        const response = await axios.get('/api/products');
         setProducts(response.data);
       } catch (error) {
         console.error('Error fetching products:', error);
@@ -18,7 +20,7 @@ const ProductList = ({ searchTerm }) => {
 
   const handleDelete = async (id) => {
     try {
-      await axios.delete(`http://localhost:5000/api/products/${id}`);
+      await axios.delete(`/api/products/${id}`);
       alert('Product deleted!');
       window.location.reload(); // Refresh
     } catch (error) {
